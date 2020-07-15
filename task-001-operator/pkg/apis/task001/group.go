345b04cd1104aed9e87217651003abfa4975dd2c
Keun Lee <keun.lee@thekeunster.com>
2020-07-15 01:22:05 -0500

    restore deleted

---
+++ task-001-operator/pkg/apis/task001/group.go
@@ -0,0 +1,6 @@
+// Package task001 contains task001 API versions.
+//
+// This file ensures Go source parsers acknowledge the task001 package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package task001
