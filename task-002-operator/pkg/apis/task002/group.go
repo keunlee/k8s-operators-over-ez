697c830068c3bcda3fe70c84d188c8eb9cabc89a
Keun Lee <keun.lee@thekeunster.com>
2020-07-29 11:48:07 -0500

    remove unnecessary projects. we will add them to our labs

--- task-002-operator/pkg/apis/task002/group.go
+++
@@ -1,6 +0,0 @@
-// Package task002 contains task002 API versions.
-//
-// This file ensures Go source parsers acknowledge the task002 package
-// and any child packages. It can be removed if any other Go source files are
-// added to this package.
-package task002
--- task-002-operator/pkg/apis/task002/v1alpha1/doc.go
+++
@@ -1,4 +0,0 @@
-// Package v1alpha1 contains API Schema definitions for the task-002 v1alpha1 API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=task-002.thekeunster.local
-package v1alpha1
